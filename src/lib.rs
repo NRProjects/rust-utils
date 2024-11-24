e59acc5ae164ea4266d5be74abc3158f3ce3b76b
Nate <nrobison1509@gmail.com>
2024-11-24 14:21:25 -0800

    Added log functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,54 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::time::SystemTime;
+
+use chrono::{DateTime, Local};
+use colored::Colorize;
+
+pub fn log(msg: impl std::fmt::Display) {
+    let time = DateTime::<Local>::from(SystemTime::now())
+        .format("%Y-%m-%d %H:%M:%S")
+        .to_string();
+    
+    println!("[{}] {} {}", 
+        time.white(), 
+        "[LOG]:".white(), 
+        msg.to_string().white()
+    );
+}
+
+pub fn warn(msg: impl std::fmt::Display) {
+    let time = DateTime::<Local>::from(SystemTime::now())
+        .format("%Y-%m-%d %H:%M:%S")
+        .to_string();
+
+    println!("[{}] {} {}", 
+        time.white(),
+        "[WARN]:".truecolor(240, 164, 93),
+        msg.to_string().white()
+    );
+}
+
+pub fn error(msg: impl std::fmt::Display) {
+    let time = DateTime::<Local>::from(SystemTime::now())
+        .format("%Y-%m-%d %H:%M:%S")
+        .to_string();
+
+    println!("[{}] {} {}", 
+        time.white(),
+        "[ERROR]:".red(),
+        msg.to_string().white()
+    );
+}
+
+pub fn success(msg: impl std::fmt::Display) {
+    let time = DateTime::<Local>::from(SystemTime::now())
+        .format("%Y-%m-%d %H:%M:%S")
+        .to_string();
+
+    println!("[{}] {} {}", 
+        time.white(),
+        "[SUCCESS]:".green(),
+        msg.to_string().white()
+    );
 }
 
 #[cfg(test)]
@@ -7,8 +56,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_logging() {
+        let number= 100;
+
+        log("Test logging");
+        warn(format!("Test warning: {}", number));
+        error("Test error");
+        success("Test success");
     }
 }
